f055661267fe5ab93e8ba4405299b329bc5009fe
Maxwell Troy Milton King <ziya@suzen.net>
2010-09-03 21:29:26 +0100

    initial project files

---
+++ csutil.spec/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("csutil.spec")]
+[assembly: AssemblyProduct("csutil.spec")]
+[assembly: AssemblyCopyright("Copyright © ziya@suzen.net 2010")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1bed5ea7-9e50-4062-b3ac-464be4845475")]
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyFileVersion("0.0.*")]
